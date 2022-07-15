7ef57c93502088198724e5f78fe997d77ceda655
OscoSon <oscarsani007@gmail.com>
2022-07-15 16:26:50 -0400

    Die Sprite Sheet Added + Die Scriptable Objects Created

---
+++ GMTK Game Jam 2022/Assets/Scripts/MagicDie.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicDie : MonoBehaviour
+{
+    public DiceSO attackDieSO;
+    //private Player player
+    void Start()
+    {
+        //player = FindObjectWithType<Player>();
+    }
+
+}
---
+++ GMTK Game Jam 2022/Assets/Scripts/RecoveryDie.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoveryDie : MonoBehaviour
+{
+    public DiceSO attackDieSO;
+    //private Player player
+    void Start()
+    {
+        //player = FindObjectWithType<Player>();
+    }
+}
