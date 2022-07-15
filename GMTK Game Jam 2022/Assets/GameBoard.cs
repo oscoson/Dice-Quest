129be0ff73b5b53b2709b974c380a5cfd698eeee
Brian Cho <b.cho@queensu.ca>
2022-07-15 11:44:30 -0700

    a

---
+++ GMTK Game Jam 2022/Assets/GameBoard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    Dictionary<Vector2, IBoardEntity> boardMap;
+
+    private void Start()
+    {
+        
+    }
+}
