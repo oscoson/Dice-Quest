69bf25feba849e3a14694aaedb796499277b8095
Brian Cho <b.cho@queensu.ca>
2022-07-16 14:57:00 -0700

    Update PieceMover.cs

--- GMTK Game Jam 2022/Assets/Scripts/PieceMover.cs
+++ GMTK Game Jam 2022/Assets/Scripts/PieceMover.cs
@@ -83,18 +83,22 @@ public class PieceMover : MonoBehaviour
 
     public void StartCombat(int index)
     {
+
         inCombat = true;
         CombatManager.Instance.StartCombat(index);
     }
 
     public void ReturnToOverworld()
     {
+
         inCombat = false;
         board.DestroyBoardEntity(nextPosition);
     }
 
     public void ExitLevel()
     {
+        CombatManager.Instance.OnCombatEnd -= ReturnToOverworld;
+
         int buildIndex = SceneManager.GetActiveScene().buildIndex;
         buildIndex++;
         buildIndex %= SceneManager.sceneCountInBuildSettings;
