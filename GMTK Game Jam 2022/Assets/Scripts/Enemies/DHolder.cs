567b65cb138dc2bbf72d1ba70c992328fd577cc0
Brian Cho <b.cho@queensu.ca>
2022-07-16 15:40:49 -0700

    Gang of four

---
+++ GMTK Game Jam 2022/Assets/Scripts/Enemies/DHolder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DHolder : Enemy
+{
+    public override void PerformAction()
+    {
+        Debug.Assert(player != null, "Need to call Init!!!!");
+        int damage = Random.Range(30, 31);
+        player.InflictDamage(damage);
+        CombatManager.Instance.UpdateCombatReportText($"{Name} attacks and damages you for " + damage.ToString() + " HP");
+    }
+}
