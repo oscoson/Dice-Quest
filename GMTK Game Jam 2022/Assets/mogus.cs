922dd9bd4377a352ce5cacf0af3ebb4bf52847bc
Brian Cho <b.cho@queensu.ca>
2022-07-16 10:09:52 -0700

    Amogus

---
+++ GMTK Game Jam 2022/Assets/mogus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mogus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit('A' + 'M' + 'O' + 'G' + 'U' + 'S');
+        // ඞ amogus
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
