7df8e8642d7ab59d0048c8b16cab3b4eabd54b77
Brian Cho <b.cho@queensu.ca>
2022-07-17 11:32:17 -0700

    Deholder balance

--- GMTK Game Jam 2022/Assets/Scripts/ScriptableObjects/DiceSO.cs
+++ GMTK Game Jam 2022/Assets/Scripts/ScriptableObjects/DiceSO.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
 
 [CreateAssetMenu(fileName = "Dice", menuName = "New Dice")]
 public class DiceSO : ScriptableObject
