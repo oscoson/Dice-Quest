50578df4eef1f1f254e8d0632ac71373d9353f37
OscoSon <oscarsani007@gmail.com>
2022-07-16 21:41:53 -0400

    w

--- GMTK Game Jam 2022/Assets/Player.cs
+++ GMTK Game Jam 2022/Assets/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public int currentHP;
     public int energyLevel = 3;
     public int maxEnergyLevel = 3;
+    public float blockValue = 0;
 
     public List<PlayableDie> diceInventory;
     private ShakeUI shaker;
@@ -24,12 +25,16 @@ public class Player : MonoBehaviour
 
     }
 
-    public void InflictDamage(int dmg)
+    public int InflictDamage(int dmg)
     {
-        currentHP -= dmg;
+        int finaldmg = (int)(dmg * (1 - blockValue));
+        Debug.Log(finaldmg);
+        currentHP -= finaldmg;
+        blockValue = 0;
         CombatManager.Instance.playAudio.Play("DamagePlayer");
         StartCoroutine(shaker.Shake(0.15f, 4f));
         if (currentHP <= 0) Die();
+        return finaldmg;
     }
 
     public int Heal(int healNum)
@@ -38,6 +43,12 @@ public class Player : MonoBehaviour
         currentHP += healAmount;
         CombatManager.Instance.UpdateCombatReportText("You healed for " + healAmount + "HP");
         return healAmount;
+    }
+    
+    public void Block()
+    {
+        blockValue += 0.2f;
+        CombatManager.Instance.UpdateCombatReportText("You prepare to block " + (blockValue * 100) + "% of damage");
     }
 
     public void AddDie(PlayableDie die)
