5eeb7e3d8477cadb9115af3029e27b20c12a5f52
Brian Cho <b.cho@queensu.ca>
2022-07-16 20:21:32 -0700

    AI Update

--- GMTK Game Jam 2022/Assets/Scripts/Enemies/BattleWiz.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Enemies/BattleWiz.cs
@@ -11,4 +11,17 @@ public class BattleWiz : Enemy
         int actualDamage = player.InflictDamage(damage);
         CombatManager.Instance.UpdateCombatReportText($"{Name} attacks and damages you for " + actualDamage.ToString() + " HP");
     }
+
+    public override void InflictDamage(int dmg)
+    {
+        float blockRatio = Random.Range(0.0f, 1.0f);
+        int blockedDamage = (int) ((float) dmg * blockRatio);
+        int actualDamage = dmg - blockedDamage;
+        currentHp -= actualDamage;
+        CombatManager.Instance.enemyAnimation.SetBool("isHit", true);
+        CombatManager.Instance.UpdateCombatReportText($"{Name} blocked {blockedDamage} damage...     you dealt {actualDamage} damage!");
+        StartCoroutine(hitAnimCancel(0.5f));
+        //EnemyHealthBar.Instance.currentHealth = currentHp;
+        if (currentHp <= 0) Die();
+    }
 }
--- GMTK Game Jam 2022/Assets/Scripts/Enemies/DHolder.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Enemies/DHolder.cs
@@ -4,11 +4,58 @@ using UnityEngine;
 
 public class DHolder : Enemy
 {
+    int damageTaken;
+    bool tripleDamage = false;
+    bool blockFull = false;
+
     public override void PerformAction()
     {
         Debug.Assert(player != null, "Need to call Init!!!!");
-        int damage = Random.Range(30, 31);
-        int actualDamage = player.InflictDamage(damage);
-        CombatManager.Instance.UpdateCombatReportText($"{Name} attacks and damages you for " + actualDamage.ToString() + " HP");
+
+        int randNum = Random.Range(0, 4);
+
+        if (tripleDamage) randNum = 0;
+        blockFull = false;
+        switch (randNum)
+        {
+            case 0:
+                int dmg = Random.Range(1, 50) * (tripleDamage ? 3 : 1);
+                tripleDamage = false;
+                int actualDamage = player.InflictDamage(dmg);
+                CombatManager.Instance.UpdateCombatReportText($"{Name} attacks and damages you for " + actualDamage.ToString() + " HP");
+                break;
+            case 1:
+                int returnedDamage = player.InflictDamage(damageTaken);
+                CombatManager.Instance.UpdateCombatReportText($"{Name} returns damage taken!    You are dealt {returnedDamage.ToString()} HP");
+                damageTaken = 0;
+                break;
+            case 2:
+                CombatManager.Instance.UpdateCombatReportText($"{Name} intends to deal triple damage next turn!");
+                tripleDamage = true;
+                break;
+            case 3:
+                CombatManager.Instance.UpdateCombatReportText($"{Name} intends to full block next turn!");
+                blockFull = true;
+                break;
+
+        }
+
+        int damage = Random.Range(1, 50);
+
+    }
+
+    public override void InflictDamage(int dmg)
+    {
+        if (!blockFull)
+        {
+            currentHp -= dmg;
+            damageTaken += dmg;
+        }
+        CombatManager.Instance.enemyAnimation.SetBool("isHit", true);
+        if (!blockFull) { CombatManager.Instance.UpdateCombatReportText("You dealed " + dmg + " damage!"); }
+        else { CombatManager.Instance.UpdateCombatReportText($"{Name} blocked your damage!"); }
+        StartCoroutine(hitAnimCancel(0.5f));
+        //EnemyHealthBar.Instance.currentHealth = currentHp;
+        if (currentHp <= 0) Die();
     }
 }
