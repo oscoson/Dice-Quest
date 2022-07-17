393f349791dd554a4471b2676e94ab350ae7474b
Brian Cho <b.cho@queensu.ca>
2022-07-17 10:04:59 -0700

    First level balancing changes

--- GMTK Game Jam 2022/Assets/Scripts/Enemies/BattleDarcherTower.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Enemies/BattleDarcherTower.cs
@@ -7,7 +7,7 @@ public class BattleDarcherTower : Enemy
     public override void PerformAction()
     {
         Debug.Assert(player != null, "Need to call Init!!!!");
-        int damage = Random.Range(5, 15);
+        int damage = Random.Range(5, 25);
         int actualDamage = player.InflictDamage(damage);
         CombatManager.Instance.UpdateCombatReportText($"{Name} attacks and damages you for " + actualDamage.ToString() + " HP");
     }
